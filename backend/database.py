04d0efdc57dcbe5811b639d25693b5aaedcfff27
Vedant-H <vedanthariyan29@gmail.com>
2025-02-24 19:45:40 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 # Connect to MongoDB
 client = MongoClient("mongodb://localhost:27017/")
-# mongodb+srv://vedanthariyan29:dV6DSY2TeeDlrN5J@cluster0.k8mod.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
 # Access the database
 db = client["hacksync"]
 
